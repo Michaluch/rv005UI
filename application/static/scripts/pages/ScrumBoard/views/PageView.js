5d810a485069c8f95886a509fb9364e28791d2f4
re0ne <re0ne@ya.ru>
2014-01-31 09:23:32 +0200

    Add require.js text plugin and folder for models and collection

--- application/static/scripts/pages/ScrumBoard/views/PageView.js
+++ application/static/scripts/pages/ScrumBoard/views/PageView.js
@@ -3,6 +3,7 @@ define(function() {
 	return Backbone.View.extend({
 		initialize: function(options) {
 		},
+		
 		render: function() {
 			$("<div>").text("Page View").appendTo(document.body);
 			return this;
