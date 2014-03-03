cad99cd92e087e6a554e00af6e60411d8605db58
QuazyThain <geiser007@gmail.com>
2014-03-03 12:48:53 +0200

    ProfilePage: Added backbone.js methods for get user info

---
+++ application/static/scripts/pages/ScrumBoard/models/User.js
@@ -0,0 +1,8 @@
+define([], 
+	function() {
+	    return Backbone.Model.extend({
+		    // idAttribute : "_id",
+		    urlRoot : "/api/user/"
+	    })
+	}
+)
--- application/static/scripts/pages/ScrumBoard/views/ProfileView.js
+++ application/static/scripts/pages/ScrumBoard/views/ProfileView.js
@@ -1,21 +1,44 @@
 define(["text!pages/ScrumBoard/templates/ProfileView.html",
-        "text!pages/ScrumBoard/templates/FooterView.html"], 
-    function(profileView, footerView) {
+        "text!pages/ScrumBoard/templates/FooterView.html",
+        "pages/ScrumBoard/models/User"],
 
+    function(profileView, footerView, UserModel) {
         return Backbone.View.extend({
             el: "body",
+            template: _.template(profileView),
         
             initialize: function(options){
+                this.user = new UserModel();
             },
                            
             render: function () {
-                this.$(".wrapper")
-                .html(profileView)
-                .append(footerView);
+                var that = this;
+                 this.user.fetch({
+                    success: function (model, response, options) {
+                        alert(model.get('email'));
+                        //this.$el.html()
+                        that.renderProfile();
+                    },
+
+                    error: function () {
+                        alert("Error");
+                    }
+                });
+
+                
+               
 
                 return this;
+            },
+
+            renderProfile: function () {
+                this.$(".wrapper")
+                .html(this.template({fname: this.user.get("fname"),
+                                    lname: this.user.get("lname"),
+                                    email: this.user.get("email"),
+                                    role: this.user.get("role")}))
+                .append(footerView);
+
             }
         })
-
 });
-
